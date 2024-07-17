4118dc140b9adf51ec18e007892f61fd7510cb8d
PeterSaan <saanpeter@gmail.com>
2024-07-17 22:27:13 +0300

    Setting up neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      	"MunifTanjim/nui.nvim",
+      	-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filsesystem toggle left<CR>", {})
+
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			enable_git_status = true,
+			window = {
+				width = 30,
+			},
+		})
+	end
+}
