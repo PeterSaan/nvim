db2007d09843a54ead188d8b7df057a221adda90
PeterSaan <saanpeter@gmail.com>
2024-10-15 01:14:47 +0300

    Add lualine and modify neotree

--- init.lua
+++ init.lua
@@ -1,4 +1,12 @@
+local map = vim.keymap.set
+
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+map("i", "<C-h>", "<Left>", {})
+map("i", "<C-l>", "<Right>", {})
+map("i", "<C-j>", "<Down>", {})
+map("i", "<C-k>", "<Up>", {})
+
 require("config.lazy")
 
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,18 +3,17 @@ return {
 	branch = "v3.x",
 	dependencies = {
 	"nvim-lua/plenary.nvim",
-	"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	"nvim-tree/nvim-web-devicons",
       	"MunifTanjim/nui.nvim",
       	-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     	},
 	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filsesystem toggle left<CR>", {})
-
 		require("neo-tree").setup({
 			enable_git_status = true,
 			window = {
 				width = 30,
 			},
 		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 	end
 }
