416b35f8a9021d402e473473e2b2d43a64cd7218
PeterSaan <saanpeter@gmail.com>
2024-11-12 23:06:44 +0200

    Took some keymaps from Primeagen and tried to fix autocompleteing

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		vim.keymap.set({"i", "n"}, "<C-p>", ":Telescope find_files<CR>", {})
+		vim.keymap.set({"i", "n"}, "<leader>pf", ":Telescope git_files<CR>", {})
+		vim.keymap.set({"i", "n"}, "<C-p>", ":Telescope git_files<CR>", {})
 	end
 }
