d5d78e8734427fbfb1fffd7546dc41d8c5c11019
PeterSaan <saanpeter@gmail.com>
2025-07-01 18:28:12 +0300

    Update clangd setup

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -3,5 +3,20 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 return {
 	capabilities = capabilities,
 	cmd = { "clangd" },
-	filetypes = { "c", "cpp", "h" }
+	filetypes = { "c", "cpp", "h" },
+	root_markers = { "compile_commands.json", ".clangd", ".git" },
+	on_init = function(client, init_result)
+		if init_result.offsetEncoding then
+			client.offset_encoding = init_result.offsetEncoding
+		end
+	end,
+	on_attach = function(_, bufnr)
+		vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdSwitchSourceHeader', function()
+			switch_source_header(bufnr)
+		end, { desc = 'Switch between source/header' })
+
+		vim.api.nvim_buf_create_user_command(bufnr, 'LspClangdShowSymbolInfo', function()
+			symbol_info()
+		end, { desc = 'Show symbol info' })
+	end,
 }
