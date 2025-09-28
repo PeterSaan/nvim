548d0998369cede7379cda4c93e569b3d8b48d00
PeterSaan <saanpeter@gmail.com>
2025-09-28 19:44:08 +0300

    Add root_dir func for arduinols

--- lsp/arduinols.lua
+++ lsp/arduinols.lua
@@ -11,4 +11,21 @@ return {
 		"-fqbn", "esp32:esp32:esp32",
 	},
 	filetypes = { "arduino" },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local dir = vim.fn.fnamemodify(fname, ":p:h")
+
+		while dir ~= "/" do
+			-- look for any .ino file in this dir
+			local inos = vim.fn.globpath(dir, "*.ino", false, true)
+			if #inos > 0 then
+				on_dir(dir)
+				return
+			end
+			dir = vim.fn.fnamemodify(dir, ":h")
+		end
+
+		-- fallback: current working directory
+		on_dir(vim.fn.getcwd())
+	end,
 }
