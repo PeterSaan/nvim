af49a4903f52e8b85549e52103e6f48ed68a39e2
PeterSaan <saanpeter@gmail.com>
2026-01-04 05:30:14 +0200

    Fix nvim-treesitter for real

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,52 @@
+local langs = {
+	"bash",
+	"c",
+	"cmake",
+	"cpp",
+	"css",
+	"dockerfile",
+	"ecma",
+	"git_config",
+	"git_rebase",
+	"gitattributes",
+	"gitcommit",
+	"gitignore",
+	"go",
+	"gomod",
+	"gosum",
+	"html",
+	"hyprlang",
+	"javascript",
+	"json",
+	"json5",
+	"jsx",
+	"kitty",
+	"make",
+	"markdown",
+	"markdown_inline",
+	"php",
+	"php_only",
+	"ssh_config",
+	"svelte",
+	"tsx",
+	"typescript",
+	"vue",
+	"yaml",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
-	config = function()
-		local ts = require("nvim-treesitter")
-
-		ts.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = langs,
+			callback = function()
+				vim.treesitter.start()
+			end,
 		})
+	end,
+	config = function()
+		require("nvim-treesitter").install(langs)
 	end,
 }
