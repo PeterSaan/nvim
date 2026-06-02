671bf91c7aa568ef2ccdf964972892e7574a7650
PeterSaan <saanpeter@gmail.com>
2026-06-02 20:19:02 +0300

    Add configuration file reading to luals

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -5,35 +5,45 @@ return {
 	capabilities = capabilities,
 	cmd = { "lua-language-server" },
 	filetypes = { "lua" },
+	root_markers = {
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		".git",
+	},
 	on_init = function(client)
 		if client.workspace_folders then
 			local path = client.workspace_folders[1].name
-			if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc')) then
+			if
+					path ~= vim.fn.stdpath("config")
+					and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
+			then
 				return
 			end
 		end
 
-		client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 			runtime = {
-				version = 'LuaJIT'
+				version = "LuaJIT",
 			},
 			workspace = {
 				checkThirdParty = false,
 				library = {
-					vim.env.VIMRUNTIME
-				}
-			}
+					vim.env.VIMRUNTIME,
+				},
+			},
 		})
 	end,
 	settings = {
 		Lua = {
 			codeLens = {
-				enable = true
+				enable = true,
 			},
 			hint = {
 				enable = true,
-				semicolon = "Disable"
-			}
-		}
-	}
+				semicolon = "Disable",
+			},
+		},
+	},
 }
